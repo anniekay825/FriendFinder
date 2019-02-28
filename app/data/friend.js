44ed27026d8467c0c3fd9ee1a5ab007ec2386349
anniekay825@gmail.com <anniekay825@gmail.com>
2019-02-27 21:01:17 -0500

    updated files

--- app/data/friend.js
+++ app/data/friend.js
@@ -1,22 +1,67 @@
-//array that holds all the 'friends' that have taken the survey
-//initialize starting with 'dummy' data
-var friendList = [
+var friendList = [{
+    name: 'Monica Geller',
+    profilePic: "https://github.com/anniekay825/FriendFinder/blob/master/app/public/images/monica.jpg",
+    scores: [
+      "5",
+      "3",
+      "4",
+      "1",
+      "5",
+      "1",
+      "2",
+      "5",
+      "5",
+      "5"
+    ]
+  },
   {
-    name: 'Stefanie Ding',
-    profilePic: 'https://avatars3.githubusercontent.com/u/17751317?v=3&s=466',
+    name: 'Rachel Green',
+    profilePic: "https://github.com/anniekay825/FriendFinder/blob/master/app/public/images/rachel.jpg",
+    scores: [
+      "1",
+      "4",
+      "5",
+      "5",
+      "5",
+      "1",
+      "3",
+      "1",
+      "5",
+      "5"
+    ]
+  },
+  {
+    name: 'Ross Geller',
+    profilePic: "app/public/images/ross.jpg",
     scores: [
       "4",
+      "4",
       "5",
+      "1",
       "5",
+      "3",
+      "3",
+      "4",
+      "5",
+      "5"
+    ]
+  },
+  {
+    name: 'Joey Tribbiani',
+    profilePic: "app/public/images/joey.jpg",
+    scores: [
+      "1",
+      "2",
       "2",
       "4",
-      "5",
-      "4",
-      "4",
       "3",
-      "4"
+      "3",
+      "5",
+      "1",
+      "5",
+      "5"
     ]
   }
-]
+];
 
 module.exports = friendList;
