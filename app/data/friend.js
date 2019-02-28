382c5ce5f878064c9d4e179f1c27c9bd7c5423e2
anniekay825@gmail.com <anniekay825@gmail.com>
2019-02-27 22:05:16 -0500

    fine tuned code

--- app/data/friend.js
+++ app/data/friend.js
@@ -1,97 +1,97 @@
 var friendList = [{
-    name: 'Monica Geller',
-    profilePic: "/app/public/images/monica.jpg",
-    scores: [
-      "5",
-      "3",
-      "4",
-      "1",
-      "5",
-      "1",
-      "2",
-      "5",
-      "5",
-      "5"
+    "name": 'Monica Geller',
+    "profilePic": "app/public/images/monica.jpg",
+    "scores":[
+      5,
+      3,
+      4,
+      1,
+      5,
+      1,
+      2,
+      5,
+      5,
+      5
     ]
   },
   {
-    name: 'Rachel Green',
-    profilePic: "/app/public/images/rachel.jpg",
-    scores: [
-      "1",
-      "4",
-      "5",
-      "5",
-      "5",
-      "1",
-      "3",
-      "1",
-      "5",
-      "5"
+    "name": 'Rachel Green',
+    "profilePic": "app/public/images/rachel.jpg",
+    "scores":[
+      1,
+      4,
+      5,
+      5,
+      5,
+      1,
+      3,
+      1,
+      5,
+      5
     ]
   },
   {
-    name: 'Ross Geller',
-    profilePic: "/app/public/images/ross.jpg",
-    scores: [
-      "4",
-      "4",
-      "5",
-      "1",
-      "5",
-      "3",
-      "3",
-      "4",
-      "5",
-      "5"
+    "name": 'Ross Geller',
+    "profilePic": "app/public/images/ross.jpg",
+    "scores": [
+      4,
+      4,
+      5,
+      1,
+      5,
+      3,
+      3,
+      4,
+      5,
+      5
     ]
   },
   {
-    name: 'Joey Tribbiani',
-    profilePic: "/app/public/images/joey.jpg",
-    scores: [
-      "1",
-      "2",
-      "2",
-      "4",
-      "3",
-      "3",
-      "5",
-      "1",
-      "5",
-      "5"
+    "name": 'Joey Tribbiani',
+    "profilePic": "app/public/images/joey.jpg",
+    "scores": [
+      1,
+      2,
+      2,
+      4,
+      3,
+      3,
+      5,
+      1,
+      5,
+      5
     ]
   },
   {
-    name: 'Chandler Bing',
-    profilePic: "/app/public/images/chandler.jpg",
-    scores: [
-      "4",
-      "3",
-      "4",
-      "2",
-      "5",
-      "1",
-      "3",
-      "3",
-      "5",
-      "5"
+    "name": 'Chandler Bing',
+    "profilePic": "app/public/images/chandler.jpg",
+    "scores": [
+      4,
+      3,
+      4,
+      2,
+      5,
+      1,
+      3,
+      3,
+      5,
+      5
     ]
   },
   {
-    name: 'Phoebe Buffay',
-    profilePic: "/app/public/images/phoebe.jpg",
-    scores: [
-      "3",
-      "4",
-      "3",
-      "3",
-      "3",
-      "5",
-      "3",
-      "2",
-      "5",
-      "5"
+    "name": 'Phoebe Buffay',
+    "profilePic": "app/public/images/phoebe.jpg",
+    "scores": [
+      3,
+      4,
+      3,
+      3,
+      3,
+      5,
+      3,
+      2,
+      5,
+      5
     ]
   }
 ];
